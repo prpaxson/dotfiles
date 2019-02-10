442f85bdc8755e9f1b3962027c39f66d9267dedd
prpaxson <prpaxson@gmail.com>
2019-02-09 22:05:36 -0800

    testing gitc in bash_profile

--- gitc/main.sh
+++
@@ -1,8 +0,0 @@
-# Combines the git add, commit, and push commands into one script
-git add -A
-if [ $# -eq 0 ]; then
-    git commit -m "Pushing Quick Commit";
-else
-    git commit -m "$*";
-fi
-git push;
